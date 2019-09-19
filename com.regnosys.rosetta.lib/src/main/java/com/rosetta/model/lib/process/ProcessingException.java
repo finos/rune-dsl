945b89ac54294aceaf3e29842602d7b61a07d634
TomCLForwood <Tom.Forwood@regnosys.com>
2019-09-19 16:00:54 +0100

    make ProcessingException usefull

--- com.regnosys.rosetta.lib/src/main/java/com/rosetta/model/lib/process/ProcessingException.java
+++ com.regnosys.rosetta.lib/src/main/java/com/rosetta/model/lib/process/ProcessingException.java
@@ -1,5 +1,7 @@
 package com.rosetta.model.lib.process;
 
+import com.rosetta.model.lib.path.RosettaPath;
+
 /**
  * @author TomForwood
  * An exception wrapper to be thrown while post processing during ingestion
@@ -9,13 +11,33 @@ public class ProcessingException extends RuntimeException {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private final String objectName;
+	private final String processorName;
+	private final RosettaPath path;
 
-	public ProcessingException(String message) {
+	public ProcessingException(String message, String objectName, String processorName, RosettaPath path) {
 		super(message);
+		this.objectName = objectName;
+		this.processorName = processorName;
+		this.path = path;
 	}
 
-	public ProcessingException(String message, Throwable cause) {
+	public ProcessingException(String message, String objectName, String processorName, RosettaPath path, Throwable cause) {
 		super(message, cause);
+		this.objectName = objectName;
+		this.processorName = processorName;
+		this.path = path;
 	}
 
+	public String getObjectName() {
+		return objectName;
+	}
+
+	public String getProcessorName() {
+		return processorName;
+	}
+
+	public RosettaPath getPath() {
+		return path;
+	}
 }
