1b1afd9696b246ac3f7486ad888091ead647b20f
TomCLForwood <38034679+TomCLForwood@users.noreply.github.com>
2020-07-07 10:20:34 +0100

    Override (#187)
    * add override flag for attributes
    
    * temporarily restore generic
    
    * better comment
    
    * restore generic type really this time

--- com.regnosys.rosetta.lib/src/main/java/com/rosetta/model/lib/GlobalKeyBuilder.java
+++ com.regnosys.rosetta.lib/src/main/java/com/rosetta/model/lib/GlobalKeyBuilder.java
@@ -4,7 +4,7 @@ import com.rosetta.model.lib.meta.MetaFieldsI;
 
 //TODO remove this useless generic - but it will create a backwards incompatibility so I will
 //remove all the usages as a preliminary step
-public interface GlobalKeyBuilder extends GlobalKey {
+public interface GlobalKeyBuilder<T> extends GlobalKey {
 
     MetaFieldsI.MetaFieldsBuilderI getMeta();
 
