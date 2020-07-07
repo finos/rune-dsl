b33283d079d44e89417362ceea60288c9ad79f8a
TomCLForwood <tom.forwood@regnosys.com>
2020-07-07 09:17:52 +0100

    better comment

--- com.regnosys.rosetta.lib/src/main/java/com/rosetta/model/lib/GlobalKeyBuilder.java
+++ com.regnosys.rosetta.lib/src/main/java/com/rosetta/model/lib/GlobalKeyBuilder.java
@@ -2,8 +2,9 @@ package com.rosetta.model.lib;
 
 import com.rosetta.model.lib.meta.MetaFieldsI;
 
-//TODO remove this useless generic
-public interface GlobalKeyBuilder<T> extends GlobalKey {
+//TODO remove this useless generic - but it will create a backwards incompatibility so I will
+//remove all the usages as a preliminary step
+public interface GlobalKeyBuilder extends GlobalKey {
 
     MetaFieldsI.MetaFieldsBuilderI getMeta();
 
