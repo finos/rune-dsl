d43c731da5d61d6fda6115094455240df6009ace
Hugo Hills <hugo.hills@regnosys.com>
2020-10-20 17:11:17 +0100

    Remove unused methods

--- com.regnosys.rosetta.lib/src/main/java/com/rosetta/util/CollectionUtils.java
+++ com.regnosys.rosetta.lib/src/main/java/com/rosetta/util/CollectionUtils.java
@@ -1,20 +1,10 @@
 package com.rosetta.util;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Supplier;
 
 public class CollectionUtils {
-
-	public static <T> T nextOrNull(Iterator<T> iterator) {
-		return iterator.hasNext() ? iterator.next() : null;
-	}
-
-	public static <T> T nextOrGet(Iterator<T> iterator, Supplier<T> supplier) {
-		return iterator.hasNext() ? iterator.next() : supplier.get();
-	}
 
 	public static <T> List<T> emptyIfNull(List<T> list) {
 		return Optional.ofNullable(list).orElseGet(() -> new ArrayList<>());
