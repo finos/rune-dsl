4650c7cf93152e68cbe07c916b0d5ed29dc51370
Hugo Hills <hugo.hills@regnosys.com>
2021-01-14 19:24:08 +0000

    Add equals and hashcode to Key and Keys classes

--- com.regnosys.rosetta.lib/src/main/java/com/rosetta/model/lib/meta/Keys.java
+++ com.regnosys.rosetta.lib/src/main/java/com/rosetta/model/lib/meta/Keys.java
@@ -1,15 +1,17 @@
 package com.rosetta.model.lib.meta;
 
-import com.google.common.collect.ImmutableList;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import com.google.common.collect.ImmutableList;
 
 public class Keys {
+	
 	private final ImmutableList<Key> keys;
 
 	public Keys(List<Key> keys) {
-		super();
-		this.keys = keys==null?ImmutableList.of():ImmutableList.copyOf(keys);
+		this.keys = keys == null ? ImmutableList.of() : ImmutableList.copyOf(keys);
 	}
 
 	public ImmutableList<Key> getKeys() {
@@ -18,18 +20,40 @@ public class Keys {
 
 	public KeysBuilder toBuilder() {
 		KeysBuilder keysBuilder = new KeysBuilder();
-		for (Key key:keys) {
+		for (Key key : keys) {
 			keysBuilder.addKey(key.toBuilder());
 		}
 		return keysBuilder;
 	}
-	
+
+	@Override 
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Keys keys1 = (Keys) o;
+		return Objects.equals(keys, keys1.keys);
+	}
+
+	@Override 
+	public int hashCode() {
+		return Objects.hash(keys);
+	}
+
+	@Override 
+	public String toString() {
+		return "Keys{" +
+				"keys=" + keys +
+				'}';
+	}
+
 	public static class KeysBuilder {
 		private List<Key.KeyBuilder> keys;
-		
+
 		public Keys build() {
-			List<Key> keysBuilt = keys!=null ? 
-					keys.stream().map(k->k.build()).collect(ImmutableList.toImmutableList()) : 
+			List<Key> keysBuilt = keys != null ?
+					keys.stream().map(k -> k.build()).collect(ImmutableList.toImmutableList()) :
 					ImmutableList.of();
 			return new Keys(keysBuilt);
 		}
@@ -37,18 +61,41 @@ public class Keys {
 		public List<Key.KeyBuilder> getKeys() {
 			return keys;
 		}
-		
+
 		public void addKey(Key.KeyBuilder key) {
-			if (keys==null) keys = new ArrayList<>();
+			if (keys == null)
+				keys = new ArrayList<>();
 			keys.add(key);
 		}
 
 		public void setKeys(List<Key.KeyBuilder> keys) {
 			this.keys = keys;
 		}
-		
+
 		public boolean hasData() {
 			return keys.stream().anyMatch(Key.KeyBuilder::hasData);
+		}
+
+		@Override 
+		public boolean equals(Object o) {
+			if (this == o)
+				return true;
+			if (o == null || getClass() != o.getClass())
+				return false;
+			KeysBuilder that = (KeysBuilder) o;
+			return Objects.equals(keys, that.keys);
+		}
+
+		@Override 
+		public int hashCode() {
+			return Objects.hash(keys);
+		}
+
+		@Override 
+		public String toString() {
+			return "KeysBuilder{" +
+					"keys=" + keys +
+					'}';
 		}
 	}
 }
