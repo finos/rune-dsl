e6ae3e6927bc4a36ad859f268deb54dae5748ba4
TomCLForwood <tom.forwood@regnosys.com>
2021-01-19 10:31:15 +0000

    more code deleted!

--- com.regnosys.rosetta.lib/src/main/java/com/rosetta/model/lib/expressions/BigDecimalExtensions.java
+++ com.regnosys.rosetta.lib/src/main/java/com/rosetta/model/lib/expressions/BigDecimalExtensions.java
@@ -7,96 +7,40 @@ import java.math.MathContext;
 
 public class BigDecimalExtensions {
 
-	/**
-	 * Add a and b
-	 */
-	private static BigDecimal add(BigDecimal a, BigDecimal b) {
-		return a.add(b);
-	}
-	
-	/**
-	 * Subtract b from a
-	 */
-	private static BigDecimal subtract(BigDecimal a, BigDecimal b) {
-		return a.subtract(b);
-	}
-
-	/**
-	 * Multiply a and b
-	 */
-	private static BigDecimal multiply(BigDecimal a, BigDecimal b) {
-		return a.multiply(b);
-	}
-
-	/**
-	 * Divide a by b 
-	 */
-	private static BigDecimal divide(BigDecimal a, BigDecimal b) {
-		return a.divide(b, MathContext.DECIMAL128);
-	}
-
-	/**
-	 * Is a close to b, with given error
-	 */
-	private static boolean closeTo(BigDecimal a, BigDecimal b, BigDecimal error) {
-		return a.subtract(b).abs().compareTo(error) < 0;
-	}
-	/**
-	 * 
-	 * @see BigDecimal.valueOf(double);
-	 */
-	public static BigDecimal valueOf(double a) {
-		return BigDecimal.valueOf(a);
-	}
-	/**
-	 * 
-	 * @see BigDecimal.valueOf(long);
-	 */
-	public static BigDecimal valueOf(long a) {
-		return BigDecimal.valueOf(a);
-	}
-	/**
-	 * 
-	 * @see BigDecimal.valueOf(long, int);
-	 */
-	public static BigDecimal valueOf(long a, int scale) {
-		return BigDecimal.valueOf(a,scale);
-	}
-	
 	/// for Mappers
 	/**
 	 * Add a and b
 	 */
 	public static Mapper<BigDecimal> add(Mapper<BigDecimal> a, Mapper<BigDecimal> b) {
-		return MapperS.of(add(a.get(),b.get()));
+		return MapperS.of(a.get().add(b.get()));
 	}
 	
 	/**
 	 * Subtract b from a
 	 */
 	public static Mapper<BigDecimal> subtract(Mapper<BigDecimal> a, Mapper<BigDecimal> b) {
-		return  MapperS.of(subtract(a.get(),b.get()));
+		return  MapperS.of(a.get().subtract(b.get()));
 	}
 	
 	/**
 	 * Multiply a and b
 	 */
 	public static Mapper<BigDecimal> multiply(Mapper<BigDecimal> a, Mapper<BigDecimal> b) {
-		return  MapperS.of(multiply(a.get(),b.get()));
+		return  MapperS.of(a.get().multiply(b.get()));
 	}
 	
 	/**
 	 * Divide a by b 
 	 */
 	public static Mapper<BigDecimal> divide(Mapper<BigDecimal> a, Mapper<BigDecimal> b) {
-		return MapperS.of(divide(a.get(),b.get()));
+		return MapperS.of(a.get().divide(b.get(), MathContext.DECIMAL128));
 	}
 	
 	/**
 	 * Is a close to b, with given error
 	 */
 	public static boolean closeTo(Mapper<BigDecimal> a, Mapper<BigDecimal> b, Mapper<BigDecimal> error) {
-		return closeTo(a.get(), b.get(), error.get());
+		return a.get().subtract(b.get()).abs().compareTo(error.get()) < 0;
 	}
 	
 	/**
@@ -106,8 +50,8 @@ public class BigDecimalExtensions {
 	public static Mapper<BigDecimal> valueOf(Mapper<? extends Number> a) {
 		Number number = a.get();
 		if (number instanceof BigDecimal)
-			return MapperS.of(valueOf(((BigDecimal) number).doubleValue()));
-		return MapperS.of(valueOf(number.longValue()));
+			return MapperS.of(BigDecimal.valueOf(((BigDecimal) number).doubleValue()));
+		return MapperS.of(BigDecimal.valueOf(number.longValue()));
 	}
 	
 	/**
@@ -115,7 +59,7 @@ public class BigDecimalExtensions {
 	 * @see BigDecimal.valueOf(long, int);
 	 */
 	public static Mapper<BigDecimal> valueOf(Mapper<Integer> a, int scale) {
-		return  MapperS.of(valueOf(a.get(), scale));
+		return  MapperS.of(BigDecimal.valueOf((long) a.get(),scale));
 	}
 	
 }
