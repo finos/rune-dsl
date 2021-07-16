70c2502b10c98ab79b07e8c0f6f32d3e6d13d5c4
James Annesley <james.annesley@regnosys.com>
2021-07-16 10:45:43 +0100

    Update ExpressionGenerator

--- com.regnosys.rosetta.lib/src/main/java/com/rosetta/model/lib/mapper/MapperBuilder.java
+++ com.regnosys.rosetta.lib/src/main/java/com/rosetta/model/lib/mapper/MapperBuilder.java
@@ -41,7 +41,7 @@ public interface MapperBuilder<T> extends Mapper<T> {
 	default <G> MapperGroupByBuilder<T,G> groupBy(Function<MapperItem<T,?>, MapperBuilder<G>> groupByFunc) {
 		Function<MapperItem<T,?>,MapperItem<G, ?>> keyFunction = 
 				i -> groupByFunc.apply(i).getItems().findFirst().get();
-		Function<MapperItem<T,?>,MapperBuilder<T>> identity = i->new MapperS<>(i);
+		Function<MapperItem<T,?>,MapperBuilder<T>> identity = MapperS::new;
 		BinaryOperator<MapperBuilder<T>> merger = MapperBuilder<T>::unionSame;
 		Map<MapperItem<G, ?>, MapperBuilder<T>> gbi = getItems()
 				.collect(Collectors.toMap(keyFunction, 
