5da815dc7957118ed7a21744e54286c774effca7
Hugo Hills <39260692+hugohills-regnosys@users.noreply.github.com>
2022-03-17 15:26:39 +0000

    Ensure new instance is created (even if already a builder)

--- com.regnosys.rosetta.lib/src/main/java/com/rosetta/model/lib/functions/RosettaFunction.java
+++ com.regnosys.rosetta.lib/src/main/java/com/rosetta/model/lib/functions/RosettaFunction.java
@@ -12,12 +12,12 @@ public interface RosettaFunction {
 	@SuppressWarnings("unchecked")
 	default <R extends RosettaModelObjectBuilder> R toBuilder(RosettaModelObject object) {
 		if (object==null) return null;
-		return (R) object.toBuilder();
+		return (R) object.build().toBuilder();
 	}
 	
 	@SuppressWarnings("unchecked")
 	default <R extends RosettaModelObjectBuilder> List<R> toBuilder(List<? extends RosettaModelObject> objects) {
 		if (objects==null) return null;
-		return  objects.stream().map(b->(R)b.toBuilder()).collect(Collectors.toList());
+		return  objects.stream().map(b->(R)b.build().toBuilder()).collect(Collectors.toList());
 	}
 }
