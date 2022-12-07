c4ab43d932c51513f98bedcb5879a1c1186e35bc
SimonCockx <47859223+SimonCockx@users.noreply.github.com>
2022-12-07 14:57:17 +0100

    Vs-code-plugin fix (#464)
    * Windows fix
    
    * Added syntax highlightning for vscode-plugin; wrote script to generate list of keywords
    
    * Cleaned
    
    * Added highlighting for numbers and comments

---
+++ com.regnosys.rosetta.tools/src/com/regnosys/rosetta/tools/ListRosettaKeywords.java
@@ -0,0 +1,30 @@
+package com.regnosys.rosetta.tools;
+
+import com.google.inject.Injector;
+import com.regnosys.rosetta.RosettaStandaloneSetup;
+import com.regnosys.rosetta.services.RosettaGrammarAccess;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.eclipse.xtext.Grammar;
+import org.eclipse.xtext.GrammarUtil;
+
+public class ListRosettaKeywords {
+
+	public static void main(String[] args) {
+		Injector inj = new RosettaStandaloneSetup().createInjectorAndDoEMFRegistration();
+		
+		RosettaGrammarAccess grammarAccess = inj.getInstance(RosettaGrammarAccess.class);
+		Grammar grammar = grammarAccess.getGrammar();
+		Set<String> keywords = GrammarUtil.getAllKeywords(grammar);
+		
+		System.out.println(
+				keywords.stream()
+					.filter(keyword -> keyword.matches(".*[a-zA-Z].*"))
+					.map(keyword -> keyword.replaceAll("-", "\\\\\\\\-"))
+					.collect(Collectors.joining("|"))
+		);
+	}
+
+}
