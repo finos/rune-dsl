2e7eada841fce16325851d5a475f99693f8763e9
Simon Cockx <simon.cockx@regnosys.com>
2023-02-23 15:02:33 +0100

    Further limited inlay hints

--- com.regnosys.rosetta.ide/src/com/regnosys/rosetta/ide/inlayhints/RosettaInlayHintsService.java
+++ com.regnosys.rosetta.ide/src/com/regnosys/rosetta/ide/inlayhints/RosettaInlayHintsService.java
@@ -4,16 +4,15 @@ import javax.inject.Inject;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.lsp4j.InlayHint;
+import org.eclipse.xtext.EcoreUtil2;
 
 import com.regnosys.rosetta.generator.java.function.CardinalityProvider;
 import com.regnosys.rosetta.rosetta.expression.ExtractAllOperation;
 import com.regnosys.rosetta.rosetta.expression.InlineFunction;
 import com.regnosys.rosetta.rosetta.expression.MapOperation;
 import com.regnosys.rosetta.rosetta.expression.ReduceOperation;
-import com.regnosys.rosetta.rosetta.expression.RosettaFeatureCall;
 import com.regnosys.rosetta.rosetta.expression.RosettaFunctionalOperation;
-import com.regnosys.rosetta.rosetta.simple.Operation;
-import com.regnosys.rosetta.rosetta.simple.Segment;
+import com.regnosys.rosetta.rosetta.simple.Function;
 import com.regnosys.rosetta.types.RType;
 import com.regnosys.rosetta.types.RosettaTypeProvider;
 
@@ -37,22 +36,16 @@ public class RosettaInlayHintsService extends AbstractInlayHintsService {
 	}
 	
 	@InlayHintCheck
-	public InlayHint checkRosettaSegment(Segment seg) {
-		if (seg.getAttribute().getCard().isPlural()) {
-			return inlayHintAfter(seg, "(0..*)", null);
-		}
-		return null;
-	}
-	
-	@InlayHintCheck
 	public InlayHint checkFunctionalOperation(RosettaFunctionalOperation op) {
-		if (op instanceof ReduceOperation || op instanceof MapOperation || op instanceof ExtractAllOperation) {
-			if (isResolved(op.getFunctionRef()) && op.getFunctionRef() instanceof InlineFunction) {
-				RType outputType = types.getRType(op);
-				boolean outputMulti = card.isMulti(op);
-	
-				if (outputType != null) {
-					return inlayHintAfter(op, typeInfo(outputType, outputMulti), null);
+		if (EcoreUtil2.getContainerOfType(op, Function.class) != null) {
+			if (op instanceof ReduceOperation || op instanceof MapOperation || op instanceof ExtractAllOperation) {
+				if (isResolved(op.getFunctionRef()) && op.getFunctionRef() instanceof InlineFunction) {
+					RType outputType = types.getRType(op);
+					boolean outputMulti = card.isMulti(op);
+		
+					if (outputType != null) {
+						return inlayHintAfter(op, typeInfo(outputType, outputMulti), null);
+					}
 				}
 			}
 		}
