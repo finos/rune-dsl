a04f58b728cc3083d9002bc4d571724e754294a3
Simon Cockx <simon.cockx@regnosys.com>
2023-03-13 16:57:43 +0100

    Cleaned

--- rosetta-ide/src/main/java/com/regnosys/rosetta/ide/inlayhints/RosettaInlayHintsService.java
+++ rosetta-ide/src/main/java/com/regnosys/rosetta/ide/inlayhints/RosettaInlayHintsService.java
@@ -2,8 +2,13 @@ package com.regnosys.rosetta.ide.inlayhints;
 
 import javax.inject.Inject;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.lsp4j.InlayHint;
 import org.eclipse.xtext.EcoreUtil2;
+import org.eclipse.xtext.Keyword;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 
 import com.regnosys.rosetta.RosettaExtensions;
 import com.regnosys.rosetta.generator.java.function.CardinalityProvider;
@@ -11,6 +16,7 @@ import com.regnosys.rosetta.rosetta.expression.MapOperation;
 import com.regnosys.rosetta.rosetta.expression.ReduceOperation;
 import com.regnosys.rosetta.rosetta.expression.RosettaFunctionalOperation;
 import com.regnosys.rosetta.rosetta.simple.Function;
+import com.regnosys.rosetta.services.RosettaGrammarAccess;
 import com.regnosys.rosetta.types.RType;
 import com.regnosys.rosetta.types.RosettaTypeProvider;
 
@@ -21,6 +27,8 @@ public class RosettaInlayHintsService extends AbstractInlayHintsService {
 	private RosettaTypeProvider types;
 	@Inject
 	private CardinalityProvider card;
+	@Inject
+	private RosettaGrammarAccess grammar;
 	
 	private String typeInfo(RType type, boolean isMulti) {
 		if (isMulti) {
@@ -32,7 +40,7 @@ public class RosettaInlayHintsService extends AbstractInlayHintsService {
 	
 	@InlayHintCheck
 	public InlayHint checkFunctionalOperation(RosettaFunctionalOperation op) {
-		if (EcoreUtil2.getContainerOfType(op, Function.class) != null) {
+		if (EcoreUtil2.getContainerOfType(op, Function.class) != null && operationHasBrackets(op)) {
 			if (op instanceof ReduceOperation || op instanceof MapOperation) {
 				if (extensions.isResolved(op.getFunction())) {
 					RType outputType = types.getRType(op);
@@ -45,5 +53,18 @@ public class RosettaInlayHintsService extends AbstractInlayHintsService {
 			}
 		}
 		return null;
+	}
+	
+	private boolean operationHasBrackets(RosettaFunctionalOperation op) {
+		Keyword keyword = grammar.getInlineFunctionAccess().getLeftSquareBracketKeyword_0_0_1();
+		ICompositeNode node = NodeModelUtils.findActualNodeFor(op);
+
+        for (INode n : node.getAsTreeIterable()) {
+            EObject ge = n.getGrammarElement();
+            if (ge instanceof Keyword && ge == keyword) {
+                return true;
+            }
+        }
+        return false;
 	}
 }
