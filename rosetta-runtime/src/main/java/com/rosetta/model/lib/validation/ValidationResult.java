3a0fc13faeaf7fa26a923bd0637f76a8d8d5d009
SimonCockx <47859223+SimonCockx@users.noreply.github.com>
2023-06-02 17:15:57 +0200

    Revert ValidationTypeEnum to remain backwards compatible (#563)

--- rosetta-runtime/src/main/java/com/rosetta/model/lib/validation/ValidationResult.java
+++ rosetta-runtime/src/main/java/com/rosetta/model/lib/validation/ValidationResult.java
@@ -34,8 +34,8 @@ public interface ValidationResult<T> {
 		return new ModelValidationResult<>(name, validationType, modelObjectName, path, definition, Optional.of(failureMessage));
 	}
 
-	enum ValidationType {
-		DATA_RULE, CHOICE_RULE, CARDINALITY, TYPE_FORMAT, KEY, ONLY_EXISTS, PRE_PROCESS_EXCEPTION, POST_PROCESS_EXCEPTION
+	enum ValidationType { // TODO: replace MODEL_INSTANCE with CARDINALITY, TYPE_FORMAT, KEY and add PRE_PROCESS_EXCEPTION. (once BSP handles validation types instead of backend)
+		DATA_RULE, CHOICE_RULE, MODEL_INSTANCE, ONLY_EXISTS, POST_PROCESS_EXCEPTION
 	}
 
 	class ModelValidationResult<T> implements ValidationResult<T> {
