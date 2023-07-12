8e58150f0ba156a18e4747bb752db0790082e6b7
hugohills-regnosys <hugo.hills@regnosys.com>
2023-07-12 11:58:42 +0100

    Add model config annotation

---
+++ rosetta-runtime/src/main/java/com/rosetta/model/lib/ModelConfig.java
@@ -0,0 +1,13 @@
+package com.rosetta.model.lib;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ModelConfig {
+    String model();
+    String type();
+}
