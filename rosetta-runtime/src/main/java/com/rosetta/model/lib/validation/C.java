1c2b9817abb12a1af411afc9f5d4df508fa070ad
PayalKhanna <payal.khanna@regnosys.com>
2024-01-15 12:13:37 +0000

    new Validation Result implementation

--- rosetta-runtime/src/main/java/com/rosetta/model/lib/validation/C.java
+++
@@ -1,9 +0,0 @@
-package com.rosetta.model.lib.validation;
-
-import com.rosetta.model.lib.path.RosettaPath;
-
-public interface C extends Validator<Foo>{
-	
-	ValidationResult validate(RosettaPath path, Foo foo);
-
-}
