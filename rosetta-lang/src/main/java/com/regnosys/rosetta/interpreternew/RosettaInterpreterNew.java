d4a4fe6c049036cba0b6be075884bbe1eb8b64ee
Jacek Kulik <j.kulik@student.tudelft.nl>
2024-05-07 12:01:16 +0200

    Added simple example interpret function

--- rosetta-lang/src/main/java/com/regnosys/rosetta/interpreternew/RosettaInterpreterNew.java
+++ rosetta-lang/src/main/java/com/regnosys/rosetta/interpreternew/RosettaInterpreterNew.java
@@ -1,7 +1,24 @@
 package com.regnosys.rosetta.interpreternew;
 
+import com.regnosys.rosetta.rosetta.expression.RosettaExpression;
+import com.regnosys.rosetta.rosetta.expression.RosettaIntLiteral;
+
 public class RosettaInterpreterNew {
 	public int Test() {
 		return 5;
+	}
+	
+	/**
+	 * Simple example interpret function to allow for better understanding of the development workflow
+	 * @param expression
+	 * @return value of RosettaIntLiteral otherwise exception
+	 */
+	public RosettaInterpreterValue interp(RosettaExpression expression) {
+		if (expression instanceof RosettaIntLiteral) {
+			RosettaIntLiteral expr = (RosettaIntLiteral)expression;
+			return new RosettaInterpreterIntegerValue(expr.getValue().intValue());
+		}
+		else
+			throw new RosettaInterpreterNewException("Unimplemented operation: " + expression.toString());
 	}
 }
