1355ed83f70b3bdff2f71a616ae6622bc1187748
Antonio Lupu <a.f.lupu@student.tudelft.nl>
2024-05-20 13:45:38 +0200

    Update RosettaInterpreterNewTest.java

--- rosetta-testing/src/test/java/com/regnosys/rosetta/interpreternew/RosettaInterpreterNewTest.java
+++ rosetta-testing/src/test/java/com/regnosys/rosetta/interpreternew/RosettaInterpreterNewTest.java
@@ -10,7 +10,6 @@ import com.regnosys.rosetta.tests.RosettaInjectorProvider;
 import com.regnosys.rosetta.interpreternew.values.RosettaInterpreterBooleanValue;
 import com.regnosys.rosetta.interpreternew.values.RosettaInterpreterIntegerValue;
 import com.regnosys.rosetta.rosetta.expression.ExpressionFactory;
-import com.regnosys.rosetta.rosetta.expression.LogicalOperation;
 import com.regnosys.rosetta.rosetta.expression.RosettaBooleanLiteral;
 import com.regnosys.rosetta.rosetta.expression.RosettaIntLiteral;
 import com.regnosys.rosetta.rosetta.interpreter.RosettaInterpreterValue;
