89c70915975ca6a632d7a3a46c75db2166ed8d41
Antonio <a.f.lupu@student.tudelft.nl>
2024-05-24 13:07:02 +0200

    Restored old files

--- rosetta-lang/src/main/java/com/regnosys/rosetta/interpreternew/visitors/RosettaInterpreterListOperationsInterpreter.java
+++ rosetta-lang/src/main/java/com/regnosys/rosetta/interpreternew/visitors/RosettaInterpreterListOperationsInterpreter.java
@@ -1,24 +1,17 @@
 package com.regnosys.rosetta.interpreternew.visitors;
 
-import java.math.BigInteger;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import com.regnosys.rosetta.interpreternew.values.RosettaInterpreterBaseValue;
 import com.regnosys.rosetta.interpreternew.values.RosettaInterpreterBooleanValue;
 import com.regnosys.rosetta.interpreternew.values.RosettaInterpreterError;
 import com.regnosys.rosetta.interpreternew.values.RosettaInterpreterErrorValue;
-import com.regnosys.rosetta.interpreternew.values.RosettaInterpreterIntegerValue;
 import com.regnosys.rosetta.interpreternew.values.RosettaInterpreterStringValue;
-import com.regnosys.rosetta.rosetta.expression.FirstOperation;
 import com.regnosys.rosetta.rosetta.expression.JoinOperation;
-import com.regnosys.rosetta.rosetta.expression.RosettaAbsentExpression;
 import com.regnosys.rosetta.rosetta.expression.RosettaContainsExpression;
-import com.regnosys.rosetta.rosetta.expression.RosettaCountOperation;
 import com.regnosys.rosetta.rosetta.expression.RosettaDisjointExpression;
-import com.regnosys.rosetta.rosetta.expression.RosettaExistsExpression;
 import com.regnosys.rosetta.rosetta.expression.RosettaExpression;
 import com.regnosys.rosetta.rosetta.interpreter.RosettaInterpreterValue;
 
@@ -141,109 +134,5 @@ public class RosettaInterpreterListOperationsInterpreter
 		
 		return new RosettaInterpreterStringValue(result);
 	}
-
-	/**
-	 * Interprets an exists operation.
-	 * If the argument of the expression is an error, it returns it.
-	 * Otherwise, it checks to see if the interpreted argument
-	 * is of single or multiple cardinality.
-	 *
-	 * @param exp Exists operation to interpret
-	 * @return Boolean indicating if the argument exists or not
-	 */
-	public RosettaInterpreterValue interp(RosettaExistsExpression exp) {
-		RosettaExpression argument = exp.getArgument();
-		RosettaInterpreterValue interpretedArgument = argument.accept(visitor);
 		
-		if (RosettaInterpreterErrorValue.errorsExist(interpretedArgument)) {
-			return interpretedArgument;
-		}
-		
-		long count = RosettaInterpreterBaseValue.valueStream(interpretedArgument).count();
-		
-		boolean exists;
-		switch (exp.getModifier()) {
-			case SINGLE:
-				exists = count == 1;
-				break;
-			case MULTIPLE:
-				exists = count > 1;
-				break;
-			default:
-				exists = count > 0;
-		}
-		
-		return new RosettaInterpreterBooleanValue(exists);
-	}
-
-	
-	/**
-	 * Interprets an "is absent" expression.
-	 * If the argument of the expression is of size 0, so:
-	 * 	- either it is optional, (0..*), and it was not instantiated
-	 *  - or it is a list with 0 elements []
-	 *
-	 * @param exp "Is absent" expression to intepret
-	 * @return Boolean indicating if the interpreted argument is absent
-	 */
-	public RosettaInterpreterValue interp(RosettaAbsentExpression exp) {
-		RosettaExpression argument = exp.getArgument();
-		RosettaInterpreterValue interpretedArgument = argument.accept(visitor);
-		
-		if (RosettaInterpreterErrorValue.errorsExist(interpretedArgument)) {
-			return interpretedArgument;
-		}
-		
-		long count = RosettaInterpreterBaseValue.valueStream(interpretedArgument).count();
-		boolean isAbsent = count == 0;
-		return new RosettaInterpreterBooleanValue(isAbsent);
-	}
-
-	/**
-	 * Interprets a count operation.
-	 * Return the number of elements in a list
-	 *
-	 * @param exp Expression to perform 'count' on
-	 * @return Integer indicating how many elements there are in the list
-	 */
-	public RosettaInterpreterValue interp(RosettaCountOperation exp) {
-		RosettaExpression argument = exp.getArgument();
-		RosettaInterpreterValue interpretedArgument = argument.accept(visitor);
-		
-		if (RosettaInterpreterErrorValue.errorsExist(interpretedArgument)) {
-			return interpretedArgument;
-		}
-		
-		long count = RosettaInterpreterBaseValue.valueStream(interpretedArgument).count();
-		return new RosettaInterpreterIntegerValue(BigInteger.valueOf(count));
-	}
-
-	/**
-	 * Interprets a first operation.
-	 * If a list is not empty, it returns the first element.
-	 * Otherwise, it returns an error.
-	 *
-	 * @param exp Expression on which to perform 'first' operation
-	 * @return First element of the list
-	 */
-	public RosettaInterpreterValue interp(FirstOperation exp) {
-		RosettaExpression argument = exp.getArgument();
-		RosettaInterpreterValue interpretedArgument = argument.accept(visitor);
-		
-		if (RosettaInterpreterErrorValue.errorsExist(interpretedArgument)) {
-			return interpretedArgument;
-		}
-		
-		long count = RosettaInterpreterBaseValue.valueStream(interpretedArgument).count();
-		if (count == 0L) {
-			// List is empty
-			return new RosettaInterpreterErrorValue(
-					new RosettaInterpreterError("List is empty"));
-		} else {
-			// List has at least one element
-			return RosettaInterpreterBaseValue.valueStream(interpretedArgument)
-					.collect(Collectors.toList()).get(0);
-		}
-	}
-
 }
--- rosetta-testing/src/test/java/com/regnosys/rosetta/interpreternew/visitors/RosettaInterpreterListOperationsInterpreterTest.java
+++ rosetta-testing/src/test/java/com/regnosys/rosetta/interpreternew/visitors/RosettaInterpreterListOperationsInterpreterTest.java
@@ -2,7 +2,6 @@ package com.regnosys.rosetta.interpreternew.visitors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.math.BigInteger;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -19,7 +18,6 @@ import com.regnosys.rosetta.interpreternew.RosettaInterpreterNew;
 import com.regnosys.rosetta.interpreternew.values.RosettaInterpreterBooleanValue;
 import com.regnosys.rosetta.interpreternew.values.RosettaInterpreterError;
 import com.regnosys.rosetta.interpreternew.values.RosettaInterpreterErrorValue;
-import com.regnosys.rosetta.interpreternew.values.RosettaInterpreterIntegerValue;
 import com.regnosys.rosetta.interpreternew.values.RosettaInterpreterStringValue;
 import com.regnosys.rosetta.rosetta.expression.ExpressionFactory;
 import com.regnosys.rosetta.rosetta.expression.RosettaExpression;
@@ -201,183 +199,5 @@ class RosettaInterpreterListOperationsInterpreterTest {
 				new RosettaInterpreterError(
 				"Logical Operation: Leftside is not of type Boolean"));
 		assertEquals(err, val);
-	}
-	
-	
-	@Test
-	void testExistsTrue() {
-		RosettaInterpreterBooleanValue expected = new RosettaInterpreterBooleanValue(true);
-		RosettaExpression expr = parser.parseExpression("(True and False) exists");
-		RosettaInterpreterValue val = interpreter.interp(expr);
-		assertTrue(val instanceof RosettaInterpreterBooleanValue);
-		RosettaInterpreterBooleanValue castedVal = (RosettaInterpreterBooleanValue)val;
-		assertEquals(expected, castedVal);
-	}
-	
-	@Test
-	void testExistsFalse() {
-		RosettaInterpreterBooleanValue expected = new RosettaInterpreterBooleanValue(false);
-		RosettaExpression expr = parser.parseExpression("[] exists");
-		RosettaInterpreterValue val = interpreter.interp(expr);
-		assertTrue(val instanceof RosettaInterpreterBooleanValue);
-		RosettaInterpreterBooleanValue castedVal = (RosettaInterpreterBooleanValue)val;
-		assertEquals(expected, castedVal);
-	}
-	
-	@Test
-	void testExistsSingleTrue() {
-		RosettaInterpreterBooleanValue expected = new RosettaInterpreterBooleanValue(true);
-		RosettaExpression expr = parser.parseExpression("[1] single exists");
-		RosettaInterpreterValue val = interpreter.interp(expr);
-		assertTrue(val instanceof RosettaInterpreterBooleanValue);
-		RosettaInterpreterBooleanValue castedVal = (RosettaInterpreterBooleanValue)val;
-		assertEquals(expected, castedVal);
-	}
-	
-	@Test
-	void testExistsSingleFalse() {
-		RosettaInterpreterBooleanValue expected = new RosettaInterpreterBooleanValue(false);
-		RosettaExpression expr = parser.parseExpression("[1,2] single exists");
-		RosettaInterpreterValue val = interpreter.interp(expr);
-		assertTrue(val instanceof RosettaInterpreterBooleanValue);
-		RosettaInterpreterBooleanValue castedVal = (RosettaInterpreterBooleanValue)val;
-		assertEquals(expected, castedVal);
-	}
-	
-	@Test
-	void testExistsMultipleTrue() {
-		RosettaInterpreterBooleanValue expected = new RosettaInterpreterBooleanValue(true);
-		RosettaExpression expr = parser.parseExpression("[1,2,3] multiple exists");
-		RosettaInterpreterValue val = interpreter.interp(expr);
-		assertTrue(val instanceof RosettaInterpreterBooleanValue);
-		RosettaInterpreterBooleanValue castedVal = (RosettaInterpreterBooleanValue)val;
-		assertEquals(expected, castedVal);
-	}
-	
-	@Test
-	void testExistsMultipleFalse() {
-		RosettaInterpreterBooleanValue expected = new RosettaInterpreterBooleanValue(false);
-		RosettaExpression expr = parser.parseExpression("[True] multiple exists");
-		RosettaInterpreterValue val = interpreter.interp(expr);
-		assertTrue(val instanceof RosettaInterpreterBooleanValue);
-		RosettaInterpreterBooleanValue castedVal = (RosettaInterpreterBooleanValue)val;
-		assertEquals(expected, castedVal);
-	}
-	
-	@Test
-	void testExistsError() {
-		RosettaInterpreterErrorValue expected = 
-				new RosettaInterpreterErrorValue(
-				new RosettaInterpreterError("Logical Operation: "
-						+ "Rightside is not of type Boolean"));
-		RosettaExpression expr = parser.parseExpression("(True and 1) single exists");
-		RosettaInterpreterValue val = interpreter.interp(expr);
-		assertTrue(val instanceof RosettaInterpreterErrorValue);
-		RosettaInterpreterErrorValue castedVal = (RosettaInterpreterErrorValue)val;
-		assertEquals(expected, castedVal);
-	}
-	
-	@Test
-	void testIsAbsentTrue() {
-		RosettaInterpreterBooleanValue expected = new RosettaInterpreterBooleanValue(true);
-		RosettaExpression expr = parser.parseExpression("[] is absent");
-		RosettaInterpreterValue val = interpreter.interp(expr);
-		assertTrue(val instanceof RosettaInterpreterBooleanValue);
-		RosettaInterpreterBooleanValue castedVal = (RosettaInterpreterBooleanValue)val;
-		assertEquals(expected, castedVal);
-	}
-	
-	@Test
-	void testIsAbsentFalse() {
-		RosettaInterpreterBooleanValue expected = new RosettaInterpreterBooleanValue(false);
-		RosettaExpression expr = parser.parseExpression("[True] is absent");
-		RosettaInterpreterValue val = interpreter.interp(expr);
-		assertTrue(val instanceof RosettaInterpreterBooleanValue);
-		RosettaInterpreterBooleanValue castedVal = (RosettaInterpreterBooleanValue)val;
-		assertEquals(expected, castedVal);
-	}
-	
-	@Test
-	void testIsAbsentError() {
-		RosettaInterpreterErrorValue expected = 
-				new RosettaInterpreterErrorValue(
-				new RosettaInterpreterError("Logical Operation: "
-						+ "Rightside is not of type Boolean"));
-		RosettaExpression expr = parser.parseExpression("(True and 1) is absent");
-		RosettaInterpreterValue val = interpreter.interp(expr);
-		assertTrue(val instanceof RosettaInterpreterErrorValue);
-		RosettaInterpreterErrorValue castedVal = (RosettaInterpreterErrorValue)val;
-		assertEquals(expected, castedVal);
-	}
-	
-	@Test
-	void testCountEmptyList() {
-		RosettaInterpreterIntegerValue expected = 
-				new RosettaInterpreterIntegerValue(BigInteger.valueOf(0));
-		RosettaExpression expr = parser.parseExpression("[] count");
-		RosettaInterpreterValue val = interpreter.interp(expr);
-		assertTrue(val instanceof RosettaInterpreterIntegerValue);
-		RosettaInterpreterIntegerValue castedVal = (RosettaInterpreterIntegerValue)val;
-		assertEquals(expected, castedVal);
-	}
-	
-	@Test
-	void testCount() {
-		RosettaInterpreterIntegerValue expected = 
-				new RosettaInterpreterIntegerValue(BigInteger.valueOf(3));
-		RosettaExpression expr = parser.parseExpression("[1, 2, 3] count");
-		RosettaInterpreterValue val = interpreter.interp(expr);
-		assertTrue(val instanceof RosettaInterpreterIntegerValue);
-		RosettaInterpreterIntegerValue castedVal = (RosettaInterpreterIntegerValue)val;
-		assertEquals(expected, castedVal);
-	}
-	
-	@Test
-	void testCountError() {
-		RosettaInterpreterErrorValue expected = 
-				new RosettaInterpreterErrorValue(
-				new RosettaInterpreterError("Logical Operation: "
-						+ "Rightside is not of type Boolean"));
-		RosettaExpression expr = parser.parseExpression("(True and 1) count");
-		RosettaInterpreterValue val = interpreter.interp(expr);
-		assertTrue(val instanceof RosettaInterpreterErrorValue);
-		RosettaInterpreterErrorValue castedVal = (RosettaInterpreterErrorValue)val;
-		assertEquals(expected, castedVal);
-	}
-	
-	@Test
-	void testFirstEmptyList() {
-		RosettaInterpreterErrorValue expected = 
-				new RosettaInterpreterErrorValue(
-				new RosettaInterpreterError("List is empty"));
-		RosettaExpression expr = parser.parseExpression("[] first");
-		RosettaInterpreterValue val = interpreter.interp(expr);
-		assertTrue(val instanceof RosettaInterpreterErrorValue);
-		RosettaInterpreterErrorValue castedVal = (RosettaInterpreterErrorValue)val;
-		assertEquals(expected, castedVal);
-	}
-	
-	@Test
-	void testFirst() {
-		RosettaInterpreterIntegerValue expected = 
-				new RosettaInterpreterIntegerValue(BigInteger.valueOf(3));
-		RosettaExpression expr = parser.parseExpression("[3, 4, 5] first");
-		RosettaInterpreterValue val = interpreter.interp(expr);
-		assertTrue(val instanceof RosettaInterpreterIntegerValue);
-		RosettaInterpreterIntegerValue castedVal = (RosettaInterpreterIntegerValue)val;
-		assertEquals(expected, castedVal);
-	}
-	
-	@Test
-	void testFirstError() {
-		RosettaInterpreterErrorValue expected = 
-				new RosettaInterpreterErrorValue(
-				new RosettaInterpreterError("Logical Operation: "
-						+ "Rightside is not of type Boolean"));
-		RosettaExpression expr = parser.parseExpression("(True and 1) first");
-		RosettaInterpreterValue val = interpreter.interp(expr);
-		assertTrue(val instanceof RosettaInterpreterErrorValue);
-		RosettaInterpreterErrorValue castedVal = (RosettaInterpreterErrorValue)val;
-		assertEquals(expected, castedVal);
 	}
 }
