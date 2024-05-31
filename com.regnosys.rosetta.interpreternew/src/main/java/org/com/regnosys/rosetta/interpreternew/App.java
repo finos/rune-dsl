5c8e5692edd31b35b90b8864f264ef1387a8ccec
Maria Cristescu <m.a.cristescu@student.tudelft.nl>
2024-05-31 18:14:10 +0200

    Setup of new project

---
+++ com.regnosys.rosetta.interpreternew/src/main/java/org/com/regnosys/rosetta/interpreternew/App.java
@@ -0,0 +1,13 @@
+package org.com.regnosys.rosetta.interpreternew;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ com.regnosys.rosetta.interpreternew/src/test/java/org/com/regnosys/rosetta/interpreternew/AppTest.java
@@ -0,0 +1,20 @@
+package org.com.regnosys.rosetta.interpreternew;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
