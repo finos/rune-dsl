3ee3ef3b1b6b7ed50114491c33a37bb243abbbcf
Jacek Kulik <j.kulik@student.tudelft.nl>
2024-06-18 09:45:17 +0200

    checkstyle fix

--- rosetta-interpreter/src/main/java/com/regnosys/rosetta/interpreternew/values/RosettaInterpreterError.java
+++ rosetta-interpreter/src/main/java/com/regnosys/rosetta/interpreternew/values/RosettaInterpreterError.java
@@ -20,12 +20,15 @@ public class RosettaInterpreterError extends MinimalEObjectImpl implements Roset
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		if (obj == null) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (getClass() != obj.getClass()) {
 			return false;
+		}
 		RosettaInterpreterError other = (RosettaInterpreterError) obj;
 		return Objects.equals(associatedObject, other.associatedObject)
 				&& Objects.equals(errorMessage, other.errorMessage);
