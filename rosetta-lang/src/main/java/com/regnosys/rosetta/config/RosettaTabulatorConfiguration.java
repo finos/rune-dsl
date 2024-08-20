1eaff076fcf8926e075845f53bbed1c1b1b7d9d7
SimonCockx <47859223+SimonCockx@users.noreply.github.com>
2024-08-20 13:36:42 +0200

    Fix null pointers in rosetta config (#810)
    * Fix
    
    * Added null checks
    
    * Cleaned
    
    * Cleaned

--- rosetta-lang/src/main/java/com/regnosys/rosetta/config/RosettaTabulatorConfiguration.java
+++ rosetta-lang/src/main/java/com/regnosys/rosetta/config/RosettaTabulatorConfiguration.java
@@ -1,13 +1,21 @@
 package com.regnosys.rosetta.config;
 
+import java.util.Collections;
 import java.util.List;
 
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.Validate;
+
 public class RosettaTabulatorConfiguration {
 	private final List<String> annotations;
 
+	public RosettaTabulatorConfiguration() {
+		this(Collections.emptyList());
+	}
 	public RosettaTabulatorConfiguration(List<String> annotations) {
+		Validate.noNullElements(annotations);
+		
 		this.annotations = annotations;
 	}
 
