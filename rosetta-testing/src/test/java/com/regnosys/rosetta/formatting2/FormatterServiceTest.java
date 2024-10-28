b7d3057f944062c998ecd4fed27fbfa9d018023a
Maria Cristescu <m.a.cristescu@student.tudelft.nl>
2024-10-28 17:31:09 +0100

    Enhanced test suite

--- rosetta-testing/src/test/java/com/regnosys/rosetta/formatting2/FormatterServiceTest.java
+++ rosetta-testing/src/test/java/com/regnosys/rosetta/formatting2/FormatterServiceTest.java
@@ -9,6 +9,7 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.xtext.serializer.ISerializer;
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.extensions.InjectionExtension;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,6 +18,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.common.io.Resources;
@@ -32,12 +34,36 @@ public class FormatterServiceTest {
 	@Test
 	void formatSingleDocument() throws IOException, URISyntaxException {
 		ResourceSet resourceSet = resourceSetProvider.get();
-		Resource resource = resourceSet.getResource(URI.createURI(Resources.getResource("formatting-test/input/test.rosetta").toString()), true);
+		Resource resource = resourceSet.getResource(URI.createURI(Resources.getResource("formatting-test/input/typeAlias.rosetta").toString()), true);
 		
 		List<Resource> formatCollection = formatterService.formatCollection(List.of(resource));
 		
-		String expected = Files.readString(Path.of(Resources.getResource("formatting-test/expected/test.rosetta").toURI()));
+		String expected = Files.readString(Path.of(Resources.getResource("formatting-test/expected/typeAlias.rosetta").toURI()));
 		String result = serializer.serialize(formatCollection.get(0).getContents().get(0));
 		assertEquals(expected, result);	
+	}
+	
+	@Test
+	void formatMultipleDocuments() throws IOException, URISyntaxException {
+		ResourceSet resourceSet = resourceSetProvider.get();
+		Resource resource1 = resourceSet.getResource(
+				URI.createURI(Resources.getResource("formatting-test/input/typeAlias.rosetta").toString()), 
+				true);
+		Resource resource2 = resourceSet.getResource(
+				URI.createURI(Resources.getResource("formatting-test/input/typeAliasWithDocumentation.rosetta").toString()), 
+				true);
+		
+		List<Resource> formatCollection = formatterService.formatCollection(List.of(resource1, resource2));
+		
+		List<String> expected = new ArrayList<>();
+		expected.add(Files.readString(Path.of(Resources.getResource("formatting-test/expected/typeAlias.rosetta").toURI())));
+		expected.add(Files.readString(Path.of(Resources.getResource("formatting-test/expected/typeAliasWithDocumentation.rosetta").toURI())));
+
+		List<String> result = new ArrayList<>();
+		for (int i = 0; i < formatCollection.size(); i++) {
+	        result.add(serializer.serialize(formatCollection.get(i).getContents().get(0)));
+	    }
+		
+		Assertions.assertIterableEquals(expected, result);	
 	}
 }
