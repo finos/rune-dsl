7cf4bd202142e7981d594f3f3bb19fdff3d0ca52
Maria Cristescu <m.a.cristescu@student.tudelft.nl>
2024-11-07 14:25:33 +0100

    Starting code for formatter plugin

---
+++ formatter-maven-plugin/src/main/java/com/regnosys/rosetta/formatter/TestMojo.java
@@ -0,0 +1,69 @@
+package com.regnosys.rosetta.formatter;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Injector;
+import com.regnosys.rosetta.RosettaStandaloneSetup;
+import com.regnosys.rosetta.formatting2.ResourceFormatterService;
+
+@Mojo(name = "format", defaultPhase = LifecyclePhase.INITIALIZE)
+public class TestMojo extends AbstractMojo{
+	private static Logger LOGGER = LoggerFactory.getLogger(TestMojo.class);
+	
+	@Parameter(property = "path", required = true)
+    private String path;
+	
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		Path directory = Paths.get(path);
+		LOGGER.info("Mojo running on path:" + directory.toString());
+		
+		Injector inj = new RosettaStandaloneSetup().createInjectorAndDoEMFRegistration();
+		ResourceSet resourceSet = inj.getInstance(ResourceSet.class);
+		ResourceFormatterService formatterService = inj.getInstance(ResourceFormatterService.class);
+		
+		try {
+            // Find all .rosetta files in the directory and load them from disk
+            List<Resource> resources = Files.walk(directory)
+                .filter(path -> path.toString().endsWith(".rosetta"))
+                .map(file -> resourceSet.getResource(URI.createFileURI(file.toString()), true))
+                .collect(Collectors.toList());
+            
+            // format resources
+            formatterService.formatCollection(resources, null);
+            
+            LOGGER.info(resources.toString());
+            
+            // save each resource
+            resources.forEach(resource -> {
+				try {
+					resource.save(null);
+					LOGGER.info("Successfully formatted and saved file at location " + resource.getURI());
+				} catch (IOException e) {
+					LOGGER.error("Error saving file at location " + resource.getURI() + ": "+ e.getMessage());
+				}
+			});
+        } catch (IOException e) {
+            LOGGER.error("Error processing files: " + e.getMessage());
+        }
+		
+	}
+	
+}
