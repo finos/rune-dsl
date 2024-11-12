54dd5941fb917eb98460b5f3c6d56d017ca0a6c6
Simon Cockx <simon.cockx@regnosys.com>
2024-11-12 15:47:37 +0100

    Proper fix

--- rosetta-tools/src/main/java/com/regnosys/rosetta/tools/modelimport/RosettaXsdParserConfig.java
+++ rosetta-tools/src/main/java/com/regnosys/rosetta/tools/modelimport/RosettaXsdParserConfig.java
@@ -1,12 +1,8 @@
 package com.regnosys.rosetta.tools.modelimport;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
 
-import org.xmlet.xsdparser.core.utils.ConfigEntryData;
 import org.xmlet.xsdparser.core.utils.DefaultParserConfig;
-import org.xmlet.xsdparser.xsdelements.XsdSchema;
-import org.xmlet.xsdparser.xsdelements.visitors.XsdSchemaVisitor;
 
 /**
  * Some built-in XSD types are missing from the default configuration. This adds them in.
@@ -22,19 +18,5 @@ public class RosettaXsdParserConfig extends DefaultParserConfig {
         xsdTypesToJava.put("base64Binary", string);
 
         return xsdTypesToJava;
-    }
-
-    // A hack for issue https://github.com/xmlet/XsdParser/issues/74
-    @Override
-    public Map<String, ConfigEntryData> getParseMappers() {
-        Map<String, ConfigEntryData> parseMappers = super.getParseMappers();
-
-        parseMappers.put(XsdSchema.TAG, new ConfigEntryData(d -> {
-            d.parserInstance.parseElements = new LinkedHashMap<>(d.parserInstance.parseElements);
-            parseMappers.put(XsdSchema.TAG, new ConfigEntryData(XsdSchema::parse, elem -> new XsdSchemaVisitor((XsdSchema) elem)));
-            return XsdSchema.parse(d);
-        }, elem -> new XsdSchemaVisitor((XsdSchema) elem)));
-
-        return parseMappers;
     }
 }
