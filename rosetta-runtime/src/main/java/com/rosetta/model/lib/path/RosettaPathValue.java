a7e6f05832fb6b8f0064914acc0e4326d83a5dc5
Minesh Patel <minesh.patel@regnosys.com>
2025-01-16 08:46:47 +0000

    Added Model Instance Flattener to create a flattened version of any model instance.

---
+++ rosetta-runtime/src/main/java/com/rosetta/model/lib/path/RosettaPathValue.java
@@ -0,0 +1,31 @@
+package com.rosetta.model.lib.path;
+
+public class RosettaPathValue {
+    private final RosettaPath path;
+    private final Object value;
+
+    public RosettaPathValue(RosettaPath path, Object value) {
+        this.path = path;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "FieldValue{" +
+                "path=" + path +
+                ", value=" + value +
+                '}';
+    }
+
+    public RosettaPath getPath() {
+        return path;
+    }
+
+    public String getArrowPath() {
+        return String.join(" -> ", path.allElementPaths());
+    }
+
+    public Object getValue() {
+        return value;
+    }
+}
