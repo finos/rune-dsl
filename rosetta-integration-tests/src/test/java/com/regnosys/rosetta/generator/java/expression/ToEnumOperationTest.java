709c842e55d3cc7f067708a4d97df93e14ac3c91
David Al-Kanani <david.al-kanani@regnosys.com>
2025-03-19 17:21:36 +0000

    test string to enum as expression

---
+++ rosetta-integration-tests/src/test/java/com/regnosys/rosetta/generator/java/expression/ToEnumOperationTest.java
@@ -0,0 +1,40 @@
+package com.regnosys.rosetta.generator.java.expression;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.inject.Inject;
+
+import org.eclipse.xtext.testing.InjectWith;
+import org.eclipse.xtext.testing.extensions.InjectionExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.regnosys.rosetta.tests.RosettaTestInjectorProvider;
+import com.regnosys.rosetta.tests.testmodel.JavaTestModel;
+import com.regnosys.rosetta.tests.testmodel.RosettaTestModelService;
+
+@ExtendWith(InjectionExtension.class)
+@InjectWith(RosettaTestInjectorProvider.class)
+public class ToEnumOperationTest {
+    @Inject
+    private RosettaTestModelService modelService;
+    
+    @Test
+    void stringToEnumTest() {
+        JavaTestModel model = modelService.toJavaTestModel("""
+                namespace test
+                
+                enum Foo:
+                    VALUE1
+                    VALUE2
+                """).compile();
+        
+        Enum<?> result = model.evaluateExpression(Enum.class, """
+                    "VALUE1" to-enum Foo
+                    """);
+        
+        var expected = model.getEnumJavaValue("Foo", "VALUE1");
+        
+        assertEquals(expected, result);
+    }
+}
