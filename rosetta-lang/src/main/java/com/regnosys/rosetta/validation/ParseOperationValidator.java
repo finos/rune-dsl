de650626eec8050be6f319ffb76538e79ee446e4
David Al-Kanani <david.al-kanani@regnosys.com>
2025-03-20 09:47:22 +0000

    refactor validator

--- rosetta-lang/src/main/java/com/regnosys/rosetta/validation/ParseOperationValidator.java
+++ rosetta-lang/src/main/java/com/regnosys/rosetta/validation/ParseOperationValidator.java
@@ -2,15 +2,12 @@ package com.regnosys.rosetta.validation;
 
 import javax.inject.Inject;
 
+import com.regnosys.rosetta.types.*;
 import org.eclipse.xtext.validation.Check;
 
 import com.regnosys.rosetta.RosettaEcoreUtil;
 import com.regnosys.rosetta.rosetta.expression.ParseOperation;
 import com.regnosys.rosetta.rosetta.expression.ToEnumOperation;
-import com.regnosys.rosetta.types.CardinalityProvider;
-import com.regnosys.rosetta.types.REnumType;
-import com.regnosys.rosetta.types.RosettaTypeProvider;
-import com.regnosys.rosetta.types.TypeSystem;
 import com.regnosys.rosetta.types.builtin.RBuiltinTypeService;
 
 import static com.regnosys.rosetta.rosetta.expression.ExpressionPackage.Literals.*;
@@ -34,14 +31,15 @@ public class ParseOperationValidator extends AbstractDeclarativeRosettaValidator
             if (cardinality.isMulti(arg)) {
                 error(String.format("The argument of %s should be of singular cardinality.'", ele.getOperator()), ele, ROSETTA_UNARY_OPERATION__ARGUMENT);
             }
-            
+
+            RMetaAnnotatedType argumentRMetaType = typeProvider.getRMetaAnnotatedType(arg);
             if (ele instanceof ToEnumOperation) {
-                if (!typeSystem.isSubtypeOf(typeProvider.getRMetaAnnotatedType(arg), builtInTypeService.UNCONSTRAINED_STRING_WITH_NO_META)
-                        && !(typeProvider.getRMetaAnnotatedType(arg).getRType() instanceof REnumType)) {
+                if (!typeSystem.isSubtypeOf(argumentRMetaType, builtInTypeService.UNCONSTRAINED_STRING_WITH_NO_META)
+                        && !(argumentRMetaType.getRType() instanceof REnumType)) {
                     error(String.format("The argument of %s should be either a string or an enum.", ele.getOperator()), ele, ROSETTA_UNARY_OPERATION__ARGUMENT);
                 }
             } else {
-                if (!typeSystem.isSubtypeOf(typeProvider.getRMetaAnnotatedType(arg), builtInTypeService.UNCONSTRAINED_STRING_WITH_NO_META)) {
+                if (!typeSystem.isSubtypeOf(argumentRMetaType, builtInTypeService.UNCONSTRAINED_STRING_WITH_NO_META)) {
                     error(String.format("he argument of  %s should be a string.", ele.getOperator()), ele, ROSETTA_UNARY_OPERATION__ARGUMENT);
                 }
             }
