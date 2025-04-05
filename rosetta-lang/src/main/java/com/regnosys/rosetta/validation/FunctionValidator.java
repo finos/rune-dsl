fdbfc18b9d65fa8fa98443c5f4614206e0e54e16
David Al-Kanani <david.al-kanani@regnosys.com>
2025-04-05 20:19:32 +0100

    save

--- rosetta-lang/src/main/java/com/regnosys/rosetta/validation/FunctionValidator.java
+++ rosetta-lang/src/main/java/com/regnosys/rosetta/validation/FunctionValidator.java
@@ -2,6 +2,7 @@ package com.regnosys.rosetta.validation;
 
 import org.eclipse.xtext.validation.Check;
 
+import com.regnosys.rosetta.rosetta.simple.Annotated;
 import com.regnosys.rosetta.rosetta.simple.Function;
 
 public class FunctionValidator extends AbstractDeclarativeRosettaValidator {
@@ -9,7 +10,17 @@ public class FunctionValidator extends AbstractDeclarativeRosettaValidator {
     
     @Check
     public void checkStaticFunctionHasImplementation(Function function) {
-        
+        if (function instanceof Annotated) {
+            var annotated = (Annotated) function;
+            if (annotated.getAnnotations()
+                .stream()
+                .map(aRef -> aRef.getAnnotation())
+                .anyMatch(a -> "staticImplementation".equals(a.getName()))) {
+                
+                System.out.println("foo");
+            }
+                
+        }
     }
     
 
