93f34a06bc2bb8f96cc44137fcb4ee3d6946039e
David Al-Kanani <david.al-kanani@regnosys.com>
2025-04-07 12:51:29 +0100

    add test

---
+++ rosetta-integration-tests/src/test/java/com/regnosys/rosetta/validation/FunctionValidatorTest.java
@@ -0,0 +1,57 @@
+package com.regnosys.rosetta.validation;
+
+import javax.inject.Inject;
+
+import org.eclipse.xtext.testing.InjectWith;
+import org.eclipse.xtext.testing.extensions.InjectionExtension;
+import org.eclipse.xtext.testing.validation.ValidationTestHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.regnosys.rosetta.tests.RosettaTestInjectorProvider;
+import com.regnosys.rosetta.tests.testmodel.RosettaTestModelService;
+import com.regnosys.rosetta.tests.util.ModelHelper;
+
+import static com.regnosys.rosetta.rosetta.simple.SimplePackage.Literals.*;
+
+@ExtendWith(InjectionExtension.class)
+@InjectWith(RosettaTestInjectorProvider.class)
+public class FunctionValidatorTest {
+    @Inject
+    private ValidationTestHelper validationTestHelper;
+    @Inject
+    private RosettaTestModelService modelService;
+    @Inject
+    private ModelHelper modelHelper;
+    
+    
+    @Test
+    void functionWithNoImplementationAndAnnotationShouldNotWarn() {
+        var model = """
+            func Foo:
+              [codeImplementation]
+              output:
+                result string (1..1)   
+            """;
+
+       var parsed = modelHelper.parseRosetta(model);
+
+       validationTestHelper.assertNoIssues(parsed);
+    }
+    
+    
+    @Test
+    void functionWithNoImplementationAndNoAnnotationShouldWarn() {
+        var model = """
+            func Foo:
+              [codeImplementation]
+              output:
+                result string (1..1)   
+            """;
+
+       var parsed = modelHelper.parseRosetta(model);
+
+       validationTestHelper.assertWarning(parsed, FUNCTION, null, "Functions with no Rune implementation should be annotated with codeImplementation");
+    }
+
+}
