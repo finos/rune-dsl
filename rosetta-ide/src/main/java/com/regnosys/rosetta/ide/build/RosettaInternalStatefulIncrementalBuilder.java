1071238fbb9657d55f5342160eb4da9bc80c4349
David Al-Kanani <david.al-kanani@regnosys.com>
2025-04-14 06:22:18 +0100

    override InternalStatefulIncrementalBuilder

---
+++ rosetta-ide/src/main/java/com/regnosys/rosetta/ide/build/RosettaInternalStatefulIncrementalBuilder.java
@@ -0,0 +1,14 @@
+package com.regnosys.rosetta.ide.build;
+
+import org.eclipse.xtext.build.IncrementalBuilder.InternalStatefulIncrementalBuilder;
+import org.eclipse.xtext.build.IncrementalBuilder.Result;
+
+public class RosettaInternalStatefulIncrementalBuilder extends InternalStatefulIncrementalBuilder {
+
+    @Override
+    public Result launch() {
+        return super.launch();
+    }
+    
+
+}
