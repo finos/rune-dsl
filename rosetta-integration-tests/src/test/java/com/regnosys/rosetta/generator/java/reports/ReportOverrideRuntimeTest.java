757d2ec658795a40017bb7d09344489b2ca2b958
Minesh Patel <minesh.patel@regnosys.com>
2025-06-03 09:26:38 +0100

    Added unittest to replicate issue

---
+++ rosetta-integration-tests/src/test/java/com/regnosys/rosetta/generator/java/reports/ReportOverrideRuntimeTest.java
@@ -0,0 +1,66 @@
+package com.regnosys.rosetta.generator.java.reports;
+
+import com.google.common.collect.Maps;
+import com.regnosys.rosetta.generator.java.RosettaJavaPackages.RootPackage;
+import com.regnosys.rosetta.generator.java.function.FunctionGeneratorHelper;
+import com.regnosys.rosetta.tests.RosettaTestInjectorProvider;
+import com.regnosys.rosetta.tests.util.CodeGeneratorTestHelper;
+import com.rosetta.model.lib.RosettaModelObject;
+import com.rosetta.util.DottedPath;
+import org.eclipse.xtext.testing.InjectWith;
+import org.eclipse.xtext.testing.extensions.InjectionExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Map;
+
+import static java.nio.file.Files.readString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(InjectionExtension.class)
+@InjectWith(RosettaTestInjectorProvider.class)
+public class ReportOverrideRuntimeTest {
+
+    @Inject
+    FunctionGeneratorHelper functionGeneratorHelper;
+    @Inject
+    CodeGeneratorTestHelper generatorTestHelper;
+
+    @Test
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    void replicateOverrideBug() throws IOException {
+        Path resourceDir = Path.of("src/test/resources/report-override-runtime-test");
+        var code = generatorTestHelper.generateCode(
+                readString(resourceDir.resolve("base.layer.rosetta")),
+                readString(resourceDir.resolve("cde.layer.price.rosetta")),
+                readString(resourceDir.resolve("cde.layer.rosetta")),
+                readString(resourceDir.resolve("common.layer.rosetta")),
+                readString(resourceDir.resolve("reg.rosetta")),
+                readString(resourceDir.resolve("ext.rosetta")));
+
+        // not sure why I need to do this
+        Map<String, String> code2 = Maps.filterKeys(code, k -> !k.endsWith("package-info"));
+
+        var classes = generatorTestHelper.compileToClasses(code2);
+
+        Class<Enum> tradeEnum = (Class<Enum>) classes.get("base.layer.TradeEnum");
+        Class<Enum> notationEnum = (Class<Enum>) classes.get("cde.layer.price.NotationEnum");
+        RosettaModelObject modelObject = generatorTestHelper.createInstanceUsingBuilder(classes, new RootPackage("base.layer"), "Instruction",
+                Map.of("id", 999, "trade", Enum.valueOf(tradeEnum, "A")));
+
+        var expectedRegTradeReportFuncResult = generatorTestHelper.createInstanceUsingBuilder(classes, new RootPackage("reg"), "RegReport", Map.of("notation", Enum.valueOf(notationEnum, "X")));
+
+        var regTradeReportFunc = functionGeneratorHelper.createFunc(classes, "RegTradeReportFunction", DottedPath.of("reg.reports"));
+        var regTradeReportFuncResult = functionGeneratorHelper.invokeFunc(regTradeReportFunc, modelObject.getType(), modelObject);
+        assertEquals(expectedRegTradeReportFuncResult, regTradeReportFuncResult);
+
+        var expectedExtRegTradeReportFuncResult = generatorTestHelper.createInstanceUsingBuilder(classes, new RootPackage("ext"), "ExtRegReport", Map.of("notation", Enum.valueOf(notationEnum, "X")));
+
+        var extRegTradeReportFunc = functionGeneratorHelper.createFunc(classes, "RegTradeExtReportFunction", DottedPath.of("ext.reports"));
+        var extRegTradeReportFuncResult = functionGeneratorHelper.invokeFunc(extRegTradeReportFunc, modelObject.getType(), modelObject);
+        assertEquals(expectedExtRegTradeReportFuncResult, extRegTradeReportFuncResult);
+    }
+}
