29a3ee2e835df2294dc32afc27ad5f17e595fc84
Maria Cristescu <maria.alexandra.cristescu@gmail.com>
2025-07-03 12:50:17 +0200

    Simplify tests by using service

--- rosetta-integration-tests/src/test/java/com/regnosys/rosetta/generator/java/function/FunctionGeneratorAliasTest.java
+++ rosetta-integration-tests/src/test/java/com/regnosys/rosetta/generator/java/function/FunctionGeneratorAliasTest.java
@@ -3,9 +3,7 @@ package com.regnosys.rosetta.generator.java.function;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.lang.reflect.Method;
 import java.util.List;
-import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -14,96 +12,102 @@ import org.eclipse.xtext.testing.extensions.InjectionExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import com.regnosys.rosetta.generator.java.RosettaJavaPackages;
 import com.regnosys.rosetta.tests.RosettaTestInjectorProvider;
-import com.regnosys.rosetta.tests.util.CodeGeneratorTestHelper;
+import com.regnosys.rosetta.tests.testmodel.RosettaTestModelService;
+import com.regnosys.rosetta.tests.util.ReflectiveInvoker;
 import com.rosetta.model.lib.RosettaModelObject;
+import com.rosetta.model.lib.functions.RosettaFunction;
 
 @ExtendWith(InjectionExtension.class)
 @InjectWith(RosettaTestInjectorProvider.class)
 public class FunctionGeneratorAliasTest {
 
 	@Inject
-	FunctionGeneratorHelper functionGeneratorHelper;
-	@Inject
-	CodeGeneratorTestHelper generatorTestHelper;
+	private RosettaTestModelService modelService;
 
 	@Test
-	void shouldAddComplexTypeListWithIfStatement() throws Exception {
-		String model = """
-					type Bar:
-						foos Foo (0..*)
-					type Foo:
-						attr string (1..1)
+	void shouldAddComplexTypeListWithIfStatement() {
+		var model = modelService.toJavaTestModel("""
+				type Bar:
+					foos Foo (0..*)
+				type Foo:
+					attr string (1..1)
 
-					func FuncFoo:
-					 	inputs:
-					 		newFoos Foo (0..*)
-					 		test boolean (1..1)
-						output:
-							bar Bar (1..1)
+				func FuncFoo:
+				 	inputs:
+				 		newFoos Foo (0..*)
+				 		test boolean (1..1)
+					output:
+						bar Bar (1..1)
 
-						add bar -> foos:
-							if test
-							then newFoos
-				""";
+					add bar -> foos:
+						if test
+						then newFoos
+				""").compile();
 
-		var code = generatorTestHelper.generateCode(model);
-		var classes = generatorTestHelper.compileToClasses(code);
-		var func = functionGeneratorHelper.createFunc(classes, "FuncFoo");
+		Class<? extends RosettaModelObject> fooClass = model.getTypeJavaClass("Foo");
+		var foo1 = model.evaluateExpression(fooClass, """
+				Foo {
+					attr: "1"
+				}
+				""");
+		var foo2 = model.evaluateExpression(fooClass, """
+				Foo {
+					attr: "2"
+				}
+				""");
 
-		var foo1 = generatorTestHelper.createInstanceUsingBuilder(classes,
-				new RosettaJavaPackages.RootPackage("com.rosetta.test.model"), "Foo", Map.of("attr", "1"));
-		var foo2 = generatorTestHelper.createInstanceUsingBuilder(classes,
-				new RosettaJavaPackages.RootPackage("com.rosetta.test.model"), "Foo", Map.of("attr", "2"));
+		Class<? extends RosettaModelObject> barClass = model.getTypeJavaClass("Bar");
+		RosettaFunction func = model.getFunctionJavaInstance("FuncFoo");
+		RosettaModelObject result = ReflectiveInvoker.from(func, "evaluate", barClass).invoke(List.of(foo1, foo2),
+				true);
 
-		var result = functionGeneratorHelper.invokeFunc(func, RosettaModelObject.class, List.of(foo1, foo2), true);
-
-		// reflective Bar.getFoos()
-		Method getFoosMethod = result.getClass().getMethod("getFoos");
 		@SuppressWarnings("unchecked")
-		List<Object> foos = (List<Object>) getFoosMethod.invoke(result);
+		List<Object> foos = (List<Object>) ReflectiveInvoker.from(result, "getFoos", List.class).invoke();
 
 		assertEquals(2, foos.size());
 		assertTrue(foos.containsAll(List.of(foo1, foo2))); // appends to existing list
 	}
 
 	@Test
-	void shouldAddComplexTypeListAliasWithIfStatement() throws Exception {
-		String model = """
-					type Bar:
-						foos Foo (0..*)
-					type Foo:
-						attr string (1..1)
+	void shouldAddComplexTypeListAliasWithIfStatement() {
+		var model = modelService.toJavaTestModel("""
+				type Bar:
+					foos Foo (0..*)
+				type Foo:
+					attr string (1..1)
 
-					func FuncFoo:
-					 	inputs:
-					 		newFoos Foo (0..*)
-					 		test boolean (1..1)
-						output:
-							bar Bar (1..1)
+				func FuncFoo:
+				 	inputs:
+				 		newFoos Foo (0..*)
+				 		test boolean (1..1)
+					output:
+						bar Bar (1..1)
 
-						alias filteredFoos: newFoos
+					add bar -> foos:
+						if test
+						then newFoos
+				""").compile();
 
-						add bar -> foos:
-							if test
-							then filteredFoos
-				""";
-		var code = generatorTestHelper.generateCode(model);
-		var classes = generatorTestHelper.compileToClasses(code);
-		var func = functionGeneratorHelper.createFunc(classes, "FuncFoo");
+		Class<? extends RosettaModelObject> fooClass = model.getTypeJavaClass("Foo");
+		var foo1 = model.evaluateExpression(fooClass, """
+				Foo {
+					attr: "1"
+				}
+				""");
+		var foo2 = model.evaluateExpression(fooClass, """
+				Foo {
+					attr: "2"
+				}
+				""");
 
-		var foo1 = generatorTestHelper.createInstanceUsingBuilder(classes,
-				new RosettaJavaPackages.RootPackage("com.rosetta.test.model"), "Foo", Map.of("attr", "1"));
-		var foo2 = generatorTestHelper.createInstanceUsingBuilder(classes,
-				new RosettaJavaPackages.RootPackage("com.rosetta.test.model"), "Foo", Map.of("attr", "2"));
+		Class<? extends RosettaModelObject> barClass = model.getTypeJavaClass("Bar");
+		RosettaFunction func = model.getFunctionJavaInstance("FuncFoo");
+		RosettaModelObject result = ReflectiveInvoker.from(func, "evaluate", barClass).invoke(List.of(foo1, foo2),
+				true);
 
-		var result = functionGeneratorHelper.invokeFunc(func, RosettaModelObject.class, List.of(foo1, foo2), true);
-
-		// reflective Bar.getFoos()
-		Method getFoosMethod = result.getClass().getMethod("getFoos");
 		@SuppressWarnings("unchecked")
-		List<Object> foos = (List<Object>) getFoosMethod.invoke(result);
+		List<Object> foos = (List<Object>) ReflectiveInvoker.from(result, "getFoos", List.class).invoke();
 
 		assertEquals(2, foos.size());
 		assertTrue(foos.containsAll(List.of(foo1, foo2))); // appends to existing list
