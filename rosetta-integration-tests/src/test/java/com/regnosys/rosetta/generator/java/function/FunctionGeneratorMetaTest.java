1826beca9e58ffbce28f5de5870a01fd7f9f1ecc
David Al-Kanani <david.al-kanani@regnosys.com>
2025-07-07 15:51:27 +0100

    fix whitespace

--- rosetta-integration-tests/src/test/java/com/regnosys/rosetta/generator/java/function/FunctionGeneratorMetaTest.java
+++ rosetta-integration-tests/src/test/java/com/regnosys/rosetta/generator/java/function/FunctionGeneratorMetaTest.java
@@ -210,7 +210,7 @@ public class FunctionGeneratorMetaTest {
         var code = generatorTestHelper.generateCode(model);
         
         var classes = generatorTestHelper.compileToClasses(code);        
-                
+        
         var myFunc = functionGeneratorHelper.createFunc(classes, "MyFunc");
         
         var result = functionGeneratorHelper.invokeFunc(myFunc, RosettaModelObject.class);
