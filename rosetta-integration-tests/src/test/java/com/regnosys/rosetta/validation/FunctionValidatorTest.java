3f410079808804b93250b28d665258293b22a245
Simon Cockx <simon@sikanda.be>
2025-09-04 11:38:34 +0200

    Added tests

--- rosetta-integration-tests/src/test/java/com/regnosys/rosetta/validation/FunctionValidatorTest.java
+++ rosetta-integration-tests/src/test/java/com/regnosys/rosetta/validation/FunctionValidatorTest.java
@@ -1,69 +1,85 @@
 package com.regnosys.rosetta.validation;
 
-import javax.inject.Inject;
-
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.extensions.InjectionExtension;
-import org.eclipse.xtext.testing.validation.ValidationTestHelper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import com.regnosys.rosetta.tests.RosettaTestInjectorProvider;
-import com.regnosys.rosetta.tests.util.ModelHelper;
-
-import static com.regnosys.rosetta.rosetta.simple.SimplePackage.Literals.*;
 
 @ExtendWith(InjectionExtension.class)
 @InjectWith(RosettaTestInjectorProvider.class)
-public class FunctionValidatorTest {
-    @Inject
-    private ValidationTestHelper validationTestHelper;
-    @Inject
-    private ModelHelper modelHelper;
-    
+public class FunctionValidatorTest extends AbstractValidatorTest {
+    @Test
+    void csvIngestionInputMayOnlyHaveAttributesOfBasicType() {
+    	assertIssues("""
+                type Input:
+                   attr string (1..1)
+                   complexAttr Foo (1..1)
+                
+                type Foo:
+                
+                func MyFunc:
+                   [ingest CSV]
+                   inputs:
+                       inp Input (1..1)
+                """, """
+                ERROR
+                """
+    		);
+    }
+
+    @Test
+    void csvProjectionOutputMayOnlyHaveAttributesOfBasicType() {
+        assertIssues("""
+                type Input:
+                   attr string (1..1)
+                   complexAttr Foo (1..1)
+                
+                type Foo:
+                
+                func MyFunc:
+                   [projection CSV]
+                   output:
+                       inp Input (1..1)
+                """, """
+                ERROR
+                """
+        );
+    }
     
     @Test
     void functionWithNoImplementationAndAnnotationShouldNotWarn() {
-        var model = """
+        assertNoIssues("""
             func Foo:
               [codeImplementation]
               output:
-                result string (1..1)   
-            """;
-
-       var parsed = modelHelper.parseRosetta(model);
-
-       validationTestHelper.assertNoIssues(parsed);
+                result string (1..1)
+            """);
     }
-    
     
     @Test
     void functionWithNoImplementationAndNoAnnotationShouldWarn() {
-        var model = """
+        assertIssues("""
             func Foo:
               output:
-                result string (1..1)   
-            """;
-
-       var parsed = modelHelper.parseRosetta(model);
-
-       validationTestHelper.assertWarning(parsed, FUNCTION, null, "A function should specify an implementation, or they should be annotated with codeImplementation");
+                result string (1..1)
+            """, """
+            WARNING (null) 'A function should specify an implementation, or they should be annotated with codeImplementation' at 4:6, length 3, on Function
+            """);
     }
 
     @Test
     void functionWithCodeImplementationAnnotationAndBodyShouldWarn() {
-        var model = """
-                func Foo:
-                  [codeImplementation]
-                  output:
-                    result string (1..1)
-                    
-                  set result: "output"  
-                """;
-
-           var parsed = modelHelper.parseRosetta(model);
-           
-           validationTestHelper.assertWarning(parsed, FUNCTION, null, "Functions annotated with codeImplementation should not have any setter operations as they will be overriden");
-
+        assertIssues("""
+            func Foo:
+              [codeImplementation]
+              output:
+                result string (1..1)
+            
+              set result: "output"
+            """, """
+            WARNING (null) 'Functions annotated with codeImplementation should not have any setter operations as they will be overriden' at 4:6, length 3, on Function
+            """);
     }
 }
