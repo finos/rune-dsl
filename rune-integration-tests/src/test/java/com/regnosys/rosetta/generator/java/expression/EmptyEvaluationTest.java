4d3ecea718c0167e9979ae1550b9396a22248500
David Al-Kanani <david.al-kanani@regnosys.com>
2025-10-10 14:25:17 +0100

    add test for empty if-then-else resolution

--- rune-integration-tests/src/test/java/com/regnosys/rosetta/generator/java/expression/EmptyEvaluationTest.java
+++ rune-integration-tests/src/test/java/com/regnosys/rosetta/generator/java/expression/EmptyEvaluationTest.java
@@ -4,11 +4,13 @@ import com.regnosys.rosetta.tests.RosettaTestInjectorProvider;
 import com.regnosys.rosetta.tests.testmodel.RosettaTestModelService;
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.extensions.InjectionExtension;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @ExtendWith(InjectionExtension.class)
 @InjectWith(RosettaTestInjectorProvider.class)
@@ -17,12 +19,21 @@ public class EmptyEvaluationTest {
     private RosettaTestModelService modelService;
 
     @Test
+    void emptyIfThenElseEvaluatesToNullTest() {
+        var model = modelService.toJavaTestModel("").compile();
+
+        Boolean result = model.evaluateExpression(Boolean.class, "if False then True");
+
+        assertNull(result);
+    }
+
+    @Test
     void emptyEvaluatesToFalseTest() {
         var model = modelService.toJavaTestModel("").compile();
 
         boolean result = model.evaluateExpression(Boolean.class, "empty or False");
 
-        Assertions.assertFalse(result);
+        assertFalse(result);
     }
 
     @Test
@@ -35,6 +46,6 @@ public class EmptyEvaluationTest {
 
         boolean result = model.evaluateExpression(Boolean.class, "Foo { alwaysFalse: False, ... } -> someBoolean or Foo { alwaysFalse: False, ... } -> alwaysFalse");
 
-        Assertions.assertFalse(result);
+        assertFalse(result);
     }
 }
