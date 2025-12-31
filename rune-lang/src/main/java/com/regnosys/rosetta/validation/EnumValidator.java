c6b831891bc5106ea4279a74a56516a286c589ba
David Al-Kanani <david.al-kanani@regnosys.com>
2025-12-31 05:48:14 +0000

    Organise imports

--- rune-lang/src/main/java/com/regnosys/rosetta/validation/EnumValidator.java
+++ rune-lang/src/main/java/com/regnosys/rosetta/validation/EnumValidator.java
@@ -1,21 +1,17 @@
 package com.regnosys.rosetta.validation;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import jakarta.inject.Inject;
-
-import org.eclipse.xtext.validation.Check;
-
 import com.regnosys.rosetta.RosettaEcoreUtil;
 import com.regnosys.rosetta.rosetta.RosettaEnumValue;
 import com.regnosys.rosetta.rosetta.RosettaEnumeration;
+import jakarta.inject.Inject;
+import org.eclipse.xtext.validation.Check;
 
-import static com.regnosys.rosetta.rosetta.RosettaPackage.Literals.*;
-import static com.regnosys.rosetta.validation.RosettaIssueCodes.*;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.regnosys.rosetta.rosetta.RosettaPackage.Literals.ROSETTA_ENUMERATION__PARENT;
+import static com.regnosys.rosetta.rosetta.RosettaPackage.Literals.ROSETTA_NAMED__NAME;
+import static com.regnosys.rosetta.validation.RosettaIssueCodes.INVALID_CASE;
 
 public class EnumValidator extends AbstractDeclarativeRosettaValidator {
 	@Inject
