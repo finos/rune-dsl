bcd1a2c1c5e2038a64bcf8e8b1b8557dc7f11c8c
Jayasri Radhakrishnan <jayasri.radhakrishnan@regnosys.com>
2026-03-10 14:31:06 +0000

    convert indentation to tabs for consistency

--- rune-lang/src/main/java/com/regnosys/rosetta/resource/RosettaModelDescription.java
+++ rune-lang/src/main/java/com/regnosys/rosetta/resource/RosettaModelDescription.java
@@ -5,20 +5,19 @@ import java.util.stream.Collectors;
 
 import com.regnosys.rosetta.rosetta.Import;
 import org.eclipse.xtext.naming.QualifiedName;
-import org.eclipse.xtext.resource.EObjectDescription;
 
 import com.regnosys.rosetta.rosetta.RosettaModel;
 
 public class RosettaModelDescription extends RosettaDescription {
     public static final String IMPORTS = "IMPORTS";
 
-    public RosettaModelDescription(QualifiedName qualifiedName, RosettaModel model, boolean isInOverriddenNamespace) {
-        super(qualifiedName, model, Map.of(IMPORTS, getImportsList(model)), isInOverriddenNamespace);
-    }
+	public RosettaModelDescription(QualifiedName qualifiedName, RosettaModel model, boolean isInOverriddenNamespace) {
+		super(qualifiedName, model, Map.of(IMPORTS, getImportsList(model)), isInOverriddenNamespace);
+	}
 
-    private static String getImportsList(RosettaModel model) {
-        return model.getImports().stream()
-                .map(Import::getImportedNamespace)
-                .collect(Collectors.joining(";"));
-    }
+	private static String getImportsList(RosettaModel model) {
+		return model.getImports().stream()
+				.map(Import::getImportedNamespace)
+				.collect(Collectors.joining(";"));
+	}
 }
