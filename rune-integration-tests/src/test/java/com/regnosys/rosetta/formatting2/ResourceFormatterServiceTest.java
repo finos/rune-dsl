ed21669ad323ea141a3206f43f2fc8b936c5ed6e
David Al-Kanani <david.al-kanani@regnosys.com>
2026-06-02 17:08:11 +0100

    Normalise line endings due to Windows line ending being added in ImportManagementService.toString(List<Import> imports)

--- rune-integration-tests/src/test/java/com/regnosys/rosetta/formatting2/ResourceFormatterServiceTest.java
+++ rune-integration-tests/src/test/java/com/regnosys/rosetta/formatting2/ResourceFormatterServiceTest.java
@@ -45,14 +45,18 @@ public class ResourceFormatterServiceTest {
 		}
 
 		for (String url : expectedUrls) {
-			expectedText.add(Files.readString(Path.of(Resources.getResource(url).toURI())));
+			expectedText.add(normalizeLineEndings(Files.readString(Path.of(Resources.getResource(url).toURI()))));
 		}
 
 		formatterService.formatCollection(resources, (resource, formattedContent) -> {
-			formattedText.add(formattedContent); // Collect formatted content for assertions
+			formattedText.add(normalizeLineEndings(formattedContent)); // Collect formatted content for assertions
 		});
 
 		Assertions.assertIterableEquals(expectedText, formattedText);
+	}
+
+	private static String normalizeLineEndings(String text) {
+		return text.replace("\r\n", "\n");
 	}
 
 	@Test
