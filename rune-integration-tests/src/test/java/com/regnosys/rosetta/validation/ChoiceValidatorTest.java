8c62249526da1846f791c74aa6ef410ff6738581
David Al-Kanani <david.al-kanani@regnosys.com>
2026-06-11 17:06:03 +0100

    Remove syntax for annotations on choice declaration and validate references to choice implied keys

--- rune-integration-tests/src/test/java/com/regnosys/rosetta/validation/ChoiceValidatorTest.java
+++ rune-integration-tests/src/test/java/com/regnosys/rosetta/validation/ChoiceValidatorTest.java
@@ -1,83 +1,95 @@
 package com.regnosys.rosetta.validation;
 
-import com.regnosys.rosetta.rosetta.RosettaModel;
-import com.regnosys.rosetta.rosetta.RosettaPackage;
-import com.regnosys.rosetta.rosetta.simple.SimplePackage;
 import com.regnosys.rosetta.tests.RosettaTestInjectorProvider;
-import com.regnosys.rosetta.tests.util.ModelHelper;
-import javax.inject.Inject;
-
-import org.eclipse.xtext.diagnostics.Severity;
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.extensions.InjectionExtension;
-import org.eclipse.xtext.testing.validation.ValidationTestHelper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(InjectionExtension.class)
 @InjectWith(RosettaTestInjectorProvider.class)
-public class ChoiceValidatorTest implements RosettaIssueCodes {
-    @Inject
-    private ValidationTestHelper validationTestHelper;
+public class ChoiceValidatorTest extends AbstractValidatorTest {
+	@Test
+	public void testChoiceOptionsDoNotOverlap() {
+		assertIssues("""
+				choice Foo:
+					Opt1
+					Nested
+				
+				type Opt1:
+				
+				choice Nested:
+					Opt1
+					Opt2
+				
+				type Opt2:
+				""",
+				"""
+				ERROR (null) 'Option 'Opt1' is already included by option 'Nested'' at 5:2, length 4, on ChoiceOption
+				"""
+		);
+	}
 
-    @Inject
-    private ModelHelper modelHelper;
+	@Test
+	void checkReferenceToFullyKeyedChoiceIsValid() {
+		assertNoIssues("""
+				type A:
+					[metadata key]
+				
+				type B:
+					[metadata key]
+				
+				choice Foo:
+					A
+					B
+				
+				type TypeToUse:
+					attr Foo (0..1)
+						[metadata reference]
+				""");
+	}
 
-    @Test
-    public void testChoiceOptionsDoNotOverlap() {
-        RosettaModel model = modelHelper.parseRosetta("""
-            choice Foo:
-                Opt1
-                Nested
-            
-            type Opt1:
-            
-            choice Nested:
-                Opt1
-                Opt2
-            
-            type Opt2:
-            """);
-        
-        validationTestHelper.assertError(model, SimplePackage.Literals.CHOICE_OPTION, null, 
-            "Option 'Opt1' is already included by option 'Nested'");
-    }
+	@Test
+	void checkReferenceToChoiceWithUnkeyedLeafIsInvalid() {
+		assertIssues("""
+				type A:
+					[metadata key]
+				
+				type B:
+				
+				choice Foo:
+					A
+					B
+				
+				type TypeToUse:
+					attr Foo (0..1)
+						[metadata reference]
+				""",
+				"""
+				ERROR (null) 'Choice 'Foo' cannot be a [metadata reference] target: every option type must be annotated with [metadata key]' at 14:7, length 3, on Attribute
+				"""
+			);
+	}
 
-    @Test
-    public void testNoCircularReferenceInChoiceOptions() {
-        RosettaModel model = modelHelper.parseRosetta("""
-            choice Foo:
-                Opt1
-                Bar
-            
-            type Opt1:
-            
-            choice Bar:
-                Foo
-            """);
-        
-        validationTestHelper.assertError(model, SimplePackage.Literals.CHOICE_OPTION, null, 
-            "Cyclic option: Foo includes Bar includes Foo");
-        validationTestHelper.assertError(model, SimplePackage.Literals.CHOICE_OPTION, null, 
-            "Cyclic option: Bar includes Foo includes Bar");
-    }
-
-    @Test
-    public void supportDeprecatedAnnotationOnChoice() {
-        RosettaModel model = modelHelper.parseRosetta("""
-            choice FooDeprecated:
-                [deprecated]
-                string
-                int
-            
-            func Foo:
-                output:
-                    result FooDeprecated (1..1)
-            
-                set result:
-                    FooDeprecated { string: "My string", ... }
-            """);
-
-        validationTestHelper.assertIssue(model, RosettaPackage.Literals.TYPE_CALL, null, Severity.INFO, "FooDeprecated is deprecated");
-    }
+	@Test
+	public void testNoCircularReferenceInChoiceOptions() {
+		assertIssues("""
+				choice Foo:
+					Opt1
+					Bar
+				
+				type Opt1:
+				
+				choice Bar:
+					Foo
+				""",
+			"""
+			ERROR (null) 'Option 'Opt1' is already included by option 'Bar'' at 5:2, length 4, on ChoiceOption
+			ERROR (null) 'Cyclic option: Foo includes Bar includes Foo' at 6:2, length 3, on ChoiceOption
+			ERROR (null) 'Duplicate option 'Bar'' at 6:2, length 3, on ChoiceOption
+			ERROR (null) 'Cyclic option: Bar includes Foo includes Bar' at 11:2, length 3, on ChoiceOption
+			ERROR (null) 'Duplicate option 'Foo'' at 11:2, length 3, on ChoiceOption
+			"""
+			);
+	}
 }
