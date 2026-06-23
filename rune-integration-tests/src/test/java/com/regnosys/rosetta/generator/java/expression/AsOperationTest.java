c1d05bdb1aa42ef2d9e2e2d77d24fa5c50b0d6f2
David Al-Kanani <david.al-kanani@regnosys.com>
2026-06-23 15:23:02 +0100

    Add safe narrowing to subtype in `navigateToChoiceOption` and enable `AsOperationTest`

--- rune-integration-tests/src/test/java/com/regnosys/rosetta/generator/java/expression/AsOperationTest.java
+++ rune-integration-tests/src/test/java/com/regnosys/rosetta/generator/java/expression/AsOperationTest.java
@@ -8,7 +8,6 @@ import javax.inject.Inject;
 
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.extensions.InjectionExtension;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
