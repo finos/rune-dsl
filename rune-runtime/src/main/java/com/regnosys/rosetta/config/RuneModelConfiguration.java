744eaaa123071fb71529b176720795b1551fb66f
David Al-Kanani <david.al-kanani@regnosys.com>
2026-07-15 14:59:07 +0100

    [9.x.x] - Default serialiser config (#1326)
    * Add default serialisation format to RuneModelConfiguration
    
    * Update comments on SerializationFormat

--- rune-runtime/src/main/java/com/regnosys/rosetta/config/RuneModelConfiguration.java
+++ rune-runtime/src/main/java/com/regnosys/rosetta/config/RuneModelConfiguration.java
@@ -7,21 +7,29 @@ import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.regnosys.rosetta.experimental.ExperimentalFeature;
+import com.rosetta.model.lib.transform.SerializationFormat;
 
 import org.apache.commons.lang3.Validate;
 
 public class RuneModelConfiguration {
 	private final String name;
 	private final List<ExperimentalFeature> enableExperimentalFeatures;
+	private final SerializationFormat defaultSerialisationFormat;
+
+	public RuneModelConfiguration(String name, List<ExperimentalFeature> enableExperimentalFeatures) {
+		this(name, enableExperimentalFeatures, null);
+	}
 
 	@JsonCreator
 	public RuneModelConfiguration(
 			@JsonProperty("name") String name,
-			@JsonProperty("enableExperimentalFeatures") List<ExperimentalFeature> enableExperimentalFeatures) {
+			@JsonProperty("enableExperimentalFeatures") List<ExperimentalFeature> enableExperimentalFeatures,
+			@JsonProperty("defaultSerialisationFormat") SerializationFormat defaultSerialisationFormat) {
 		Objects.requireNonNull(name);
 		this.name = name;
 		this.enableExperimentalFeatures = enableExperimentalFeatures == null ? Collections.emptyList() : enableExperimentalFeatures;
 		Validate.noNullElements(this.enableExperimentalFeatures);
+		this.defaultSerialisationFormat = defaultSerialisationFormat;
 	}
 
 	public String getName() {
@@ -30,5 +38,9 @@ public class RuneModelConfiguration {
 
 	public List<ExperimentalFeature> getEnableExperimentalFeatures() {
 		return enableExperimentalFeatures;
+	}
+
+	public SerializationFormat getDefaultSerialisationFormat() {
+		return defaultSerialisationFormat;
 	}
 }
